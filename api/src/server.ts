a20891c5f6e29d28e770a24b0e5058f80a0f6292
Saad Syed <saadsyed950@gmail.com>
2025-04-25 16:09:28 +0530

    Deployed on hostinger

--- api/src/server.ts
+++ api/src/server.ts
@@ -20,7 +20,7 @@ app.use(
             'http://localhost:8081',
             'http://localhost:5173',
             'https://admin-ariss.vercel.app',
-            'https://lern.nxtribe.com/',
+            'https://lern.nxtribe.com',
         ],
         credentials: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
