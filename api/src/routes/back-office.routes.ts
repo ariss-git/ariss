445012cc60e9f126fc29becd0abf8a342b1f76bd
Saad Sayyed <saadsyed950@gmail.com>
2025-08-30 10:48:04 +0530

    Imports fixed in controller and routes of back office

--- api/src/routes/back-office.routes.ts
+++ api/src/routes/back-office.routes.ts
@@ -1,25 +1,20 @@
 // src/routes/back-office.routes.ts
 
 import { authMiddleware } from '../middleware/auth.middleware.js';
-import {
-    registerBackOfficeController,
-    isBackOfficeSignedInController,
-    approveBackOfficeController,
-    getAllApprovedBackOfficesController,
-    getAllDisapprovedBackOfficesController,
-    disapproveBackOfficeController,
-    getAllBackofficesForDealerController,
-} from '../controllers/back-office.controller.js';
+import * as backOfficeControllers from '../controllers/back-office.controller.js';
 import { Router } from 'express';
 
 const backOfficeRoutes = Router();
 
-backOfficeRoutes.post('/register', registerBackOfficeController);
-backOfficeRoutes.get('/check', authMiddleware, isBackOfficeSignedInController);
-backOfficeRoutes.put('/approve/:dealer_id/:backoffice_id', approveBackOfficeController);
-backOfficeRoutes.get('/approved/:dealer_id', getAllApprovedBackOfficesController);
-backOfficeRoutes.get('/disapproved/:dealer_id', getAllDisapprovedBackOfficesController);
-backOfficeRoutes.put('/disapprove/:dealer_id/:backoffice_id', disapproveBackOfficeController);
-backOfficeRoutes.get('/approvals/:dealer_id', getAllBackofficesForDealerController);
+backOfficeRoutes.post('/register', backOfficeControllers.registerBackOfficeController);
+backOfficeRoutes.get('/check', authMiddleware, backOfficeControllers.isBackOfficeSignedInController);
+backOfficeRoutes.put('/approve/:dealer_id/:backoffice_id', backOfficeControllers.approveBackOfficeController);
+backOfficeRoutes.get('/approved/:dealer_id', backOfficeControllers.getAllApprovedBackOfficesController);
+backOfficeRoutes.get('/disapproved/:dealer_id', backOfficeControllers.getAllDisapprovedBackOfficesController);
+backOfficeRoutes.put(
+    '/disapprove/:dealer_id/:backoffice_id',
+    backOfficeControllers.disapproveBackOfficeController
+);
+backOfficeRoutes.get('/approvals/:dealer_id', backOfficeControllers.getAllBackofficesForDealerController);
 
 export default backOfficeRoutes;
