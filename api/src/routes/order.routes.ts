89101fca25bda953f327368cd1be947127645d25
Saad Sayyed <saadsyed950@gmail.com>
2025-08-30 12:05:35 +0530

    Order API refactored and documented

--- api/src/routes/order.routes.ts
+++ api/src/routes/order.routes.ts
@@ -1,9 +1,12 @@
+// src/routes/order.routes.ts
+
 import { Router } from 'express';
 import { createOrderController } from '../controllers/order.controller.js';
 
 const orderRoutes = Router();
 
-// Route for creating a new order
+// @route   POST /create
+// @desc    Route to create a new order
 orderRoutes.post('/create', createOrderController);
 
 export default orderRoutes;
