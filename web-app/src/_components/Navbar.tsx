143bafc9e3eedd2037b9f85521040b74dca91be7
Saad Sayyed <saadsyed950@gmail.com>
2025-09-19 20:01:25 +0530

    Sheet for mobile

--- web-app/src/_components/Navbar.tsx
+++ web-app/src/_components/Navbar.tsx
@@ -1,6 +1,24 @@
 import { ModeToggle } from '../components/mode-toggle';
-import { Bell, PanelLeftClose, PanelRightClose } from 'lucide-react';
+import {
+    ArrowLeftRight,
+    Bell,
+    BookCheck,
+    Landmark,
+    LayoutDashboard,
+    Menu,
+    NotebookPen,
+    Package2,
+    PanelLeftClose,
+    PanelRightClose,
+    ShoppingCart,
+    TicketPercent,
+    Users,
+} from 'lucide-react';
 import AdminAccount from './AdminAccount';
+import { Sheet, SheetContent, SheetTrigger } from '../components/ui/sheet';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '../components/ui/accordion';
 
 interface NavbarProps {
     toggleSidebar: () => void;
@@ -16,6 +34,7 @@ const Navbar = ({ toggleSidebar, isSidebarOpen }: NavbarProps) => {
                         Ariss
                     </h1> */}
                     {/* <img className="lg:max-w-32" src="/Ariss_Logo.png" alt="Logo" /> */}
+                    <MobileSheet />
                 </div>
 
                 <div className="flex justify-center items-center lg:gap-x-2">
@@ -33,11 +52,108 @@ const Navbar = ({ toggleSidebar, isSidebarOpen }: NavbarProps) => {
                         )}
                     </button>
                     <ModeToggle />
-                    <Bell size={16} className="text-stone-100 dark:text-stone-100 stroke-[1.5] lg:mr-6" />
+                    <Bell
+                        size={16}
+                        className="text-stone-100 dark:text-stone-100 stroke-[1.5] lg:mr-6 mr-3"
+                    />
                     <AdminAccount />
                 </div>
             </div>
         </nav>
+    );
+};
+
+const MobileSheet = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const closeSheet = () => {
+        setIsOpen(false);
+    };
+    return (
+        <div className="block lg:hidden">
+            <Sheet open={isOpen} onOpenChange={setIsOpen}>
+                <SheetTrigger asChild>
+                    <button>
+                        <Menu
+                            onClick={() => setIsOpen(true)}
+                            size={16}
+                            className="text-stone-100 dark:text-stone-100 stroke-[1.5]"
+                        />
+                    </button>
+                </SheetTrigger>
+                <SheetContent
+                    side="left"
+                    className="flex flex-col gap-y-6 justify-start items-start h-full w-full bg-black text-[#ABB9E8] font-work px-6 py-10 text-xl overflow-y-auto"
+                >
+                    <Link className="flex items-center gap-x-2" onClick={closeSheet} to="/">
+                        <LayoutDashboard size={16} className="mr-2" /> Dashboard
+                    </Link>
+                    <Accordion type="single" collapsible>
+                        <AccordionItem value="item-1">
+                            <AccordionTrigger className="flex items-center gap-x-2 text-xl">
+                                <Users size={16} className="mr-2" /> Customers
+                            </AccordionTrigger>
+                            <AccordionContent className="flex justify-start items-start flex-col gap-y-4 text-base mt-4">
+                                <Link onClick={closeSheet} to="/customers">
+                                    All Customers
+                                </Link>
+                                <Link onClick={closeSheet} to="/customers/distributors">
+                                    Distributors
+                                </Link>
+                                <Link onClick={closeSheet} to="/customers/dealers/approved">
+                                    Approved Dealers
+                                </Link>
+                                <Link onClick={closeSheet} to="/customers/dealers/not-approved">
+                                    Disapproved Dealers
+                                </Link>
+                                <Link onClick={closeSheet} to="/customers/technicians">
+                                    Technicians
+                                </Link>
+                                <Link onClick={closeSheet} to="/customers/backoffices">
+                                    Backoffices
+                                </Link>
+                            </AccordionContent>
+                        </AccordionItem>
+                    </Accordion>
+                    <Accordion type="single" collapsible>
+                        <AccordionItem value="item-2">
+                            <AccordionTrigger className="flex items-center gap-x-2 text-xl">
+                                <Package2 size={16} className="mr-2" />
+                                Products
+                            </AccordionTrigger>
+                            <AccordionContent className="flex justify-start items-start flex-col gap-y-4 text-base mt-4">
+                                <Link onClick={closeSheet} to="/categories">
+                                    Categories
+                                </Link>
+                                <Link onClick={closeSheet} to="/subcategories">
+                                    Subcategories
+                                </Link>
+                                <Link onClick={closeSheet} to="/products">
+                                    Products
+                                </Link>
+                            </AccordionContent>
+                        </AccordionItem>
+                    </Accordion>
+                    <Link className="flex items-center gap-x-2" onClick={closeSheet} to="/discounts">
+                        <TicketPercent size={16} className="mr-2" /> Discounts
+                    </Link>
+                    <Link className="flex items-center gap-x-2" onClick={closeSheet} to="/rma">
+                        <ArrowLeftRight size={16} className="mr-2" /> RMA
+                    </Link>
+                    <Link className="flex items-center gap-x-2" onClick={closeSheet} to="/orders">
+                        <ShoppingCart size={16} className="mr-2" /> Orders
+                    </Link>
+                    <Link className="flex items-center gap-x-2" onClick={closeSheet} to="/invoices">
+                        <Landmark size={16} className="mr-2" /> Invoices
+                    </Link>
+                    <Link className="flex items-center gap-x-2" onClick={closeSheet} to="/courses">
+                        <BookCheck size={16} className="mr-2" /> Courses
+                    </Link>
+                    <Link className="flex items-center gap-x-2" onClick={closeSheet} to="/tests">
+                        <NotebookPen size={16} className="mr-2" /> Tests
+                    </Link>
+                </SheetContent>
+            </Sheet>
+        </div>
     );
 };
 
