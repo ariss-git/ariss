fc1d5cf5481cb2891a509d9aa78cb47d62304e10
Saad Sayyed <saadsyed950@gmail.com>
2025-09-20 11:57:22 +0530

    Controller to create panel user

---
+++ api/src/controllers/panel-user.controller.ts
@@ -0,0 +1,28 @@
+// src/services/panel-user.controller.ts
+
+import { PanelUserService } from '@/services/panel-user.service.js';
+import { Request, Response } from 'express';
+
+const panelUserServices = new PanelUserService();
+
+export const createPanelUserController = async (req: Request, res: Response) => {
+    const { panelId, email, name, profilePic, panelType } = req.body;
+
+    if (!panelId || !email || !name || !panelType) {
+        return res.status(404).json({ success: false, message: 'All fields are required' });
+    }
+
+    try {
+        const panelUser = await panelUserServices.createPanelUserService(
+            panelId,
+            email,
+            name,
+            profilePic,
+            panelType
+        );
+
+        return res.status(201).json({ success: true, data: panelUser });
+    } catch (error: any) {
+        return res.status(500).json({ success: false, message: error.message });
+    }
+};
