87afd50feef6743f8fb3e140381894db16f4813b
Saad Sayyed <saadsyed950@gmail.com>
2025-09-22 23:11:08 +0530

    All sorts of fetch courses routes

--- api/src/routes/course.routes.ts
+++ api/src/routes/course.routes.ts
@@ -4,5 +4,8 @@ import { Router } from 'express';
 const courseRoutes = Router();
 
 courseRoutes.post('/', courseControllers.createCourseController);
+courseRoutes.get('/', courseControllers.fetchAllCoursesController);
+courseRoutes.get('/:courseId', courseControllers.fetchSingleCourseController);
+courseRoutes.get('/active', courseControllers.fetchAllActiveCoursesController);
 
 export default courseRoutes;
