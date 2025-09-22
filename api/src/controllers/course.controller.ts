e1d775fb8cf85127a1cc4a6f73941aaa582fbfd0
Saad Sayyed <saadsyed950@gmail.com>
2025-09-22 23:10:27 +0530

    All sorts of fetch courses controllers

--- api/src/controllers/course.controller.ts
+++ api/src/controllers/course.controller.ts
@@ -17,3 +17,36 @@ export const createCourseController = async (req: Request, res: Response) => {
         return res.status(500).json({ success: false, message: error.message });
     }
 };
+
+export const fetchAllCoursesController = async (_req: Request, res: Response) => {
+    try {
+        const course = await courseServices.fetchAllCourses();
+        return res.status(200).json({ success: true, data: course });
+    } catch (error: any) {
+        return res.status(400).json({ success: false, message: error.message });
+    }
+};
+
+export const fetchSingleCourseController = async (req: Request, res: Response) => {
+    const { courseId } = req.params;
+
+    if (!courseId) {
+        return res.status(404).json({ success: false, message: 'Course ID not found in params' });
+    }
+
+    try {
+        const course = await courseServices.fetchSingleCourse(courseId);
+        return res.status(200).json({ success: true, data: course });
+    } catch (error: any) {
+        return res.status(400).json({ success: false, message: error.message });
+    }
+};
+
+export const fetchAllActiveCoursesController = async (_req: Request, res: Response) => {
+    try {
+        const course = await courseServices.fetchActiveCourses();
+        return res.status(200).json({ success: true, data: course });
+    } catch (error: any) {
+        return res.status(400).json({ success: false, message: error.message });
+    }
+};
