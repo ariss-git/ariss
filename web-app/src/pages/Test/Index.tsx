473d680b854969a968d87861f03fd7a6efc75852
Saad Sayyed <saadsyed950@gmail.com>
2025-09-23 16:27:38 +0530

    Course module done

---
+++ web-app/src/pages/Test/Index.tsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+import { ArrowLeft } from 'lucide-react';
+import FetchAllCourses from '../Courses/FetchAllCourses';
+
+const Test = () => {
+    return (
+        <div className="flex flex-col lg:gap-y-6 justify-start w-full h-full lg:p-12 bg-transparent lg:px-4 lg:py-8">
+            <h1 className="font-work text-left text-[16px] font-semibold capitalize dark:text-stone-100 text-[#495057] flex items-center">
+                <Link to="/">
+                    <ArrowLeft className="w-4 h-4 mr-2 " />
+                </Link>
+                All Tests:
+            </h1>
+            <FetchAllCourses />
+        </div>
+    );
+};
+
+export default Test;
