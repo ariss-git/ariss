b26b5d3bc246d03cb231cd9c1306142223f06105
Saad Sayyed <saadsyed950@gmail.com>
2025-09-27 20:01:03 +0530

    Working on mobile new app

--- api/dist/routes/payment.routes.js
+++
@@ -1,8 +0,0 @@
-// src/routes/payment.routes.ts
-import { Router } from 'express';
-import * as paymentControllers from '../controllers/payment.controller.js';
-const paymentRoutes = Router();
-// @route   POST /verify
-// @desc    Route to verify Razorpay payment
-paymentRoutes.post('/verify', paymentControllers.verifyPaymentController);
-export default paymentRoutes;
--- api/dist/routes/test.routes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import * as testControllers from '../controllers/test.controller.js';
-const testRoutes = Router();
-testRoutes.post('/', testControllers.addQuestionController);
-testRoutes.get('/', testControllers.getAllQuestionsController);
-testRoutes.delete('/:testId', testControllers.deleteQuestionController);
-export default testRoutes;
